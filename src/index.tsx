1c6c3bcc097167646256703aa7313a1e4eed9829
DouglasB834 <douglas_borges09@hotmail.com>
2022-11-01 20:51:22 -0300

    feat: add StyleGlobal

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { StyleGlobal } from "./styles/globalStyle";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <StyleGlobal/>
     <App />
   </React.StrictMode>
 );
