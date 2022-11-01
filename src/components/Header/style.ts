7a58d1d4607c4d894592635ba5cb9428b1342bd5
DouglasB834 <douglas_borges09@hotmail.com>
2022-11-01 20:02:08 -0300

    feat: add  Heander Component

---
+++ src/components/Header/style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.div`
+    width: 100%;
+    height: 10vh;
+    background-color: #000;
+    color: #fff;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+`;
