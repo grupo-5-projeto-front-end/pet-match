9e0193e222fb006d959b6bcc56950d30af6ab0d3
Igor <silveira.igor@outlook.com>
2022-11-02 15:49:55 -0300

    feat: add usercontext

---
+++ src/services/requests/patchUser.ts
@@ -0,0 +1,22 @@
+import { iUser } from "../../contexts/UserContext";
+import { api } from "../api";
+
+export interface iBodyPatchUser {
+  name?: string;
+  city?: string;
+  adress?: string;
+  email?: string;
+  password?: string;
+  tel?: string;
+  avatar?: string | null;
+  favoritePets?: [];
+}
+
+export const patchUser = async (
+  id: number,
+  body: iBodyPatchUser
+): Promise<iUser> => {
+  const { data } = await api.patch(`/users/${id}`, body);
+
+  return data;
+};
