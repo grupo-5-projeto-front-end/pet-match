fbe4a46fd995e527acc62b0d63f9afbead0b9853
DouglasB834 <douglas_borges09@hotmail.com>
2022-11-02 13:46:41 -0300

    style: change form component

--- src/components/FormBase/style.ts
+++ src/components/FormBase/style.ts
@@ -1,19 +1,13 @@
 import styled from "styled-components";
 
-export const StyleFormBase = styled.form`
+export const StyledFormBase = styled.form`
   width: 100%;
   padding: 1rem;
   max-width: 516px;
   border-radius: 5px;
-  margin: 0 auto;
-
-  background-color: #000;
+  
+  background-color: var(--color-black);
 
   display: flex;
   flex-direction: column;
-
-  label{
-    color: #fff;
-    margin-bottom: .2rem;
-  }
 `;
