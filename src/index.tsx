6e775f33d8cb68cdd485ca19186f768effaa427b
Vinicius <v.nascimento23@yahoo.com.br>
2022-11-02 14:34:12 -0300

    feat: add initial toast container config

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import App from "./App";
 import { StyledGlobal } from "./styles/globalStyle";
 
@@ -11,7 +12,18 @@ root.render(
   <React.StrictMode>
     <StyledGlobal/>
     <BrowserRouter>
-    <App />
+      <App />
+      <ToastContainer
+        position="top-left"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </BrowserRouter>
   </React.StrictMode>
 );
