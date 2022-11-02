30a5a0de8eae86fce5d513ef518c6a87572b39b8
DouglasB834 <douglas_borges09@hotmail.com>
2022-11-02 11:37:16 -0300

    feat: add basic heander component

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const StyledHeader = styled.div`
+export const StyledHeader = styled.header`
     width: 100%;
     height: 10vh;
     background-color: #000;
