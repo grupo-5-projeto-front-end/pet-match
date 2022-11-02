eaf7f3ed9407c7ded53b9959d48be2c689e94e97
DouglasB834 <douglas_borges09@hotmail.com>
2022-11-02 10:58:32 -0300

    feat: add Form Component base

---
+++ src/components/FormBase/style.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const StyleFormBase = styled.form`
+  width: 100%;
+  padding: 1rem;
+  max-width: 516px;
+  border-radius: 5px;
+  margin: 0 auto;
+
+  background-color: #000;
+
+  display: flex;
+  flex-direction: column;
+
+  label{
+    color: #fff;
+    margin-bottom: .2rem;
+  }
+`;
