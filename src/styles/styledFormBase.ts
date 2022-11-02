9859b9e9894511bebf9e062a1ca4a4802c7eb689
DouglasB834 <douglas_borges09@hotmail.com>
2022-11-02 16:35:46 -0300

    fix: change components location

--- src/styles/styledFormBase.ts
+++ src/styles/styledFormBase.ts
@@ -2,12 +2,12 @@ import styled from "styled-components";
 
 export const StyledFormBase = styled.form`
   width: 100%;
-  padding: 1rem;
+  padding: 2rem;
   max-width: 516px;
   border-radius: 5px;
-  
   background-color: var(--color-black);
-
+  
+  gap: 20px;
   display: flex;
   flex-direction: column;
 `;
