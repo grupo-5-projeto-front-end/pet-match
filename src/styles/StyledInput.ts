dd81fd265cfbdcf1603df4e1f48b51140b378a08
matheuscg13 <matheusdecastrogoncalves@gmail.com>
2022-11-02 14:44:22 -0300

    feat: add StyledInput

---
+++ src/styles/StyledInput.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyledInput = styled.input`
+    width: 90%;
+    height: 35px;
+    background-color: var(--color-gray);
+    outline: none;
+    border: transparent;
+    border-radius: 4px;
+    padding: 15px;
+    font-size: 20px;
+`
