9976bead96a0f8a296174599c6650c24512ac6eb
Vinicius <v.nascimento23@yahoo.com.br>
2022-11-02 13:20:51 -0300

    feat: add error span for forms in the application

--- src/components/Button/style.ts
+++ src/components/Button/style.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { iPropsButtom } from "./interface";
 
-export const  StyleButton = styled.button<iPropsButtom>`
+export const StyleButton = styled.button<iPropsButtom>`
 
 width: ${({width})=> `${width}px` };
 
