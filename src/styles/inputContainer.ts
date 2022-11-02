af2f7b3c7031d45a72dcf01f0d4b01eee3d5834e
Vinicius <v.nascimento23@yahoo.com.br>
2022-11-02 14:23:39 -0300

    feat: add input containers for forms

---
+++ src/styles/inputContainer.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const StyledInputContainer = styled.div`
+    width: 90%;
+
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 10px;
+
+    label {
+        font-weight: 400;
+        font-size: 1rem;
+        color: var(--color-white);
+    }
+`
