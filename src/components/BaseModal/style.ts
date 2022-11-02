fd7552655e4a37eaf1a5e4a20e654dd0b4cfc17f
DouglasB834 <douglas_borges09@hotmail.com>
2022-11-02 12:50:49 -0300

    feat: add baseModal component

---
+++ src/components/BaseModal/style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const StyleBaseModal = styled.div`
+inset: 0;
+background-color: hsla(0, 0%, 0%, 0.701) ;
+
+position: absolute;
+top: 0;
+
+display: flex;
+align-items: center;
+justify-content: center;
+
+`
