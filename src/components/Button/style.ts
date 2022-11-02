49eea93ba874977f2025d289427b7bf20de4b724
DouglasB834 <douglas_borges09@hotmail.com>
2022-11-01 21:36:46 -0300

    feat: add button compont

---
+++ src/components/Button/style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { iPropsButtom } from "./interface";
+
+export const  StyleButton = styled.button<iPropsButtom>`
+
+width: ${({width})=> `${width}px` };
+
+background: var(${({background})=>background });
+
+height: ${({height})=>`${height}px` };
+
+`
