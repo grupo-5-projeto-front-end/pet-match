6d56448db7e665fe234e461499ae3d32b37977fe
matheuscg13 <matheusdecastrogoncalves@gmail.com>
2022-11-03 17:23:43 -0300

    feat: add not found page

---
+++ src/pages/NotFoundPage/index.tsx
@@ -0,0 +1,39 @@
+import { StyledHeader } from "../../styles/header";
+import PetMatchLogo from "../../assets/PETMATCH.png";
+import { StyledButton } from "../../components/Button/style";
+import sad_dog from "../../assets/sad_dog.webp"
+import { StyledNotFoundPage } from "./style";
+import { Footer } from "../../components/Footer";
+import { useNavigate } from "react-router-dom";
+
+export const NotFoundPage = () => {
+
+  const navigate = useNavigate()
+
+  return (
+    <StyledNotFoundPage>
+      <StyledHeader>
+        <img src={PetMatchLogo} alt="Logo PatchMatch" />
+      </StyledHeader>
+      <div>
+      <main>
+        <figure>
+          <img src={sad_dog} alt="" />
+        </figure>
+        <h1>O número da cachorrinha não está aqui :(</h1>
+        <StyledButton
+          width="150px"
+          background="--color-salmon"
+          color="--color-white"
+          fontSize="2"
+          height="50"
+          onClick={() => navigate(-1)}
+        >
+          Voltar
+        </StyledButton>
+      </main>
+      </div>
+      <Footer></Footer>
+    </StyledNotFoundPage>
+  );
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import { LoginPage } from '../pages/LoginPage';
-
+import { NotFoundPage } from '../pages/NotFoundPage';
 export const MainRoutes = () => (
   <Routes>
     <Route path={"/"} element={<h1>Landing Page</h1>}/>
@@ -8,6 +8,6 @@ export const MainRoutes = () => (
     <Route path={"/register"} element={<h1>Register</h1>} />
     <Route path={"/dashboard"} element={<h1>Dashboard</h1>} />
     <Route path={"/dashboard/pet"} element={<h1>Visualizar Pet</h1>} />
-    <Route path={"*"} element={<h1>Rota não autorizada</h1>}/>
+    <Route path={"*"} element={<NotFoundPage/>}/>
   </Routes>
 )
