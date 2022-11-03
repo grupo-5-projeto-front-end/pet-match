a8210064e7565515a67bb968200da6da3582f58b
DouglasB834 <douglas_borges09@hotmail.com>
2022-11-03 18:17:41 -0300

    feat: add function modal edit perfil

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
 import { MainRoutes } from "./routes";
 import { UserProvider } from "./contexts/UserContext";
-import { useModalContext } from "./contexts/ModalContext";
-import { EditPerfil } from "./components/EditPerfil";
 
 function App() {
-
-  const {modal, openModal} = useModalContext()
-
   return (
-    <>   
+    <>
       <UserProvider>
-
-      <button onClick={()=>openModal()}>chama modal</button>
-    { 
-    modal && ( <EditPerfil/>)
-    }
         <MainRoutes />
       </UserProvider>
     </>
