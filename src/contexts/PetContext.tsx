2fe832bf30fd630f8a481206bd86587079df4d1e
Igor <silveira.igor@outlook.com>
2022-11-03 10:15:11 -0300

    feat: add createpet in  petcontext

--- src/contexts/PetContext.tsx
+++ src/contexts/PetContext.tsx
@@ -1,13 +1,37 @@
 import { createContext, ReactNode, useContext } from "react";
+import { api } from "../services/api";
 
 interface iPetProps {
   children: ReactNode;
 }
 
+interface iCreatePetBody {
+  userId: number;
+  name: string;
+  sex: string;
+  category: string;
+  breed: string;
+  age: string;
+  bio: string;
+  avatar: string;
+  image: string[];
+  visible: boolean;
+}
+
 export const PetContext = createContext({});
 
 export const PetProvider = ({ children }: iPetProps) => {
-  return <PetContext.Provider value={{}}>{children}</PetContext.Provider>;
+  const createPet = async (body: iCreatePetBody): Promise<void> => {
+    try {
+      await api.post("/pets", body);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <PetContext.Provider value={{ createPet }}>{children}</PetContext.Provider>
+  );
 };
 
 export const usePetContext = () => {
