2ac1099d99f64c57edb9babbbaf3f89186373194
Vinicius <v.nascimento23@yahoo.com.br>
2022-11-05 13:53:35 -0300

    fix: add some fixes to not found page

--- src/pages/NotFoundPage/style.ts
+++ src/pages/NotFoundPage/style.ts
@@ -2,55 +2,55 @@ import styled from "styled-components"
 
 export const StyledNotFoundPage = styled.div`
 
-
-gap: 10px;
-height: 100vh;
-div{
-    
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    height: 75vh;
-    }
-
-    header{
-        img{
-            max-height: 100%;
-        }
-    }
-
-    main{
+    gap: 10px;
+    height: 83vh;
+        
+    div{
+        
         display: flex;
         flex-direction: column;
-        align-items: center;
         justify-content: center;
-        background-color: var(--color-black);
-        padding: 8vh;
-        gap: 10px;
-        height: 45vh;
-       
-
-        h1{
-            color: var(--color-white);
-            font-size: 1rem;
+        height: 75vh;
         }
 
-        figure{
-            
-            width: 100%;
-            max-height: 90%;
-            display: flex;
-            justify-content: center;
-
+        header{
             img{
-                border: solid 4px var(--color-salmon);
-                border-radius: 50%;
-                
-                
+                max-height: 100%;
+            }
+        }
+
+        main{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            background-color: var(--color-black);
+            padding: 8vh;
+            gap: 10px;
+            height: 45vh;
+        
+
+            h1{
+                color: var(--color-white);
+                font-size: 1rem;
             }
 
+            figure{
+                
+                width: 100%;
+                max-height: 90%;
+                display: flex;
+                justify-content: center;
+
+                img{
+                    border: solid 4px var(--color-salmon);
+                    border-radius: 50%;
+                    
+                    
+                }
+
+            }
+
+
         }
-
-
-    }
 `
