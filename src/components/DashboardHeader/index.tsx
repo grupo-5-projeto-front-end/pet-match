c19526dfa22fa30bf6e7807fca5d4c7179a34104
Lucas Vaz <lucasvazo@hotmail.com>
2022-11-07 18:32:55 -0300

    feat: user logout

--- src/components/DashboardHeader/index.tsx
+++ src/components/DashboardHeader/index.tsx
@@ -1,12 +1,22 @@
+import { useNavigate } from "react-router-dom";
 import { StyledHeader } from "../../styles/header"
 import { StyledButton } from "../Button/style";
 import UserMenu from "../UserMenu";
 
 export const DashboardHeader = () => {
+
+    const navigate = useNavigate()
+
+    const userLogout = () => {
+        window.localStorage.clear()
+        navigate('/')
+    }
+
+
     return (
         <StyledHeader>
             <UserMenu/>
-            <StyledButton className="logoutButton" fontSize="1.2" color="--color-white" width="70px" height="40" background="--color-salmon">Sair</StyledButton>
+            <StyledButton onClick={userLogout} className="logoutButton" fontSize="1.2" color="--color-white" width="70px" height="40" background="--color-salmon">Sair</StyledButton>
         </StyledHeader>
     );
 };
