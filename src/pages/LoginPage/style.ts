bbc4c959325a3f6dc17577383f8f771948bb54f8
Vinicius <v.nascimento23@yahoo.com.br>
2022-11-07 18:36:21 -0300

    feat: add link button style as a component

--- src/pages/LoginPage/style.ts
+++ src/pages/LoginPage/style.ts
@@ -1,6 +1,4 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
-import { iStyledLinkProps } from "../../components/UserMenu/interface";
 
 export const StyledLoginPage = styled.div`
   header {
@@ -114,32 +112,5 @@ export const StyledLoginPage = styled.div`
       gap: 20%;
       padding: 0 5%;
     }
-  }
-`;
-
-
-export const StyledLinkRedirect = styled(Link)<iStyledLinkProps>`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  margin: 0;
-
-  width: ${({ width }) => `${width}`};
-
-  background: var(${({ background }) => background});
-  color: var(${({ color }) => color});
-
-  height: ${({ height }) => `${height}px`};
-
-  font-size: ${({ fontSize }) => `${fontSize}rem`};
-  border-radius: 5px;
-  font-weight: 700;
-  transition: 0.2s linear;
-
-  cursor: pointer;
-
-  :hover {
-    filter: brightness(1.5);
   }
 `;
