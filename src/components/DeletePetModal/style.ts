00b28200fe25329c4b04ec41ff844fe158bc6719
GregStoz <greg_stoz@outlook.com>
2022-11-09 17:02:09 -0300

    fix: add gap to delete modal and change favicon

--- src/components/DeletePetModal/style.ts
+++ src/components/DeletePetModal/style.ts
@@ -3,20 +3,10 @@ import { StyledFormBase } from "../../styles/styledFormBase";
 
 export const StyledFormPet = styled(StyledFormBase)`
   width: 90%;
-  gap: 6px;
+  gap: 64px;
   animation-duration: 1s;
 
-  input {
-    width: 100%;
-    border-radius: 5px;
-    padding: 3px 5px;
-    font-size: 1rem;
-    color: var(--color-placeholder);
-    background-color: var(--color-gray);
-  }
-  .userId {
-    display: none;
-  }
+  
 `;
 
 export const StyledTitleAddPet = styled.div`
@@ -32,52 +22,3 @@ export const StyledTitleAddPet = styled.div`
   }
 `;
 
-export const StyledSexAndCategory = styled.div`
-  width: 100%;
-  display: flex;
-  select {
-    font-size: 1rem;
-    border-radius: 5px;
-    height: 45px;
-    background-color: var(--color-gray);
-  }
-
-  label {
-    color: var(--color-white);
-    margin-bottom: 1px;
-  }
-  span {
-    color: var(--color-salmon);
-  }
-
-  .category {
-    display: flex;
-    flex-direction: column;
-    width: 70%;
-    select {
-      width: 98%;
-    }
-    label {
-      color: var(--color-white);
-      margin-bottom: 1px;
-    }
-
-    @media all and (min-width: 480px) {
-      input {
-        width: 98%;
-      }
-      margin-right: 10px;
-      width: 74%;
-    }
-  }
-  .sex {
-    display: flex;
-    flex-direction: column;
-    select {
-      font-size: 1rem;
-      border-radius: 5px;
-      height: 45px;
-      background-color: var(--color-gray);
-    }
-  }
-`;
